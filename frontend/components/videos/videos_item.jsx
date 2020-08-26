56b27d29134e676c8a3006f5c90069d8f2960f4b
thewizard23 <seanpscott6@gmail.com>
2020-08-26 16:15:24 -0700

    video css

--- frontend/components/videos/videos_item.jsx
+++ frontend/components/videos/videos_item.jsx
@@ -27,13 +27,19 @@ class VideosItem extends React.Component {
         return (
             <div className="video-show">
                 <h1 className="video-title">{this.props.video.title}</h1>
-                <video id="video-test" className="video-trailer" 
-                onMouseEnter={this.handleMouseEnter.bind(this)}
-                onMouseLeave={this.handleMouseLeave.bind(this)}
-                width="219" 
-                height="123">
-                    <source src={`${this.props.video.videoUrl}#t=3`} type="video/mp4" ></source>
-                </video>
+                <div className="video-full">
+                    <video id="video-test" className="video-trailer" 
+                    onMouseEnter={this.handleMouseEnter.bind(this)}
+                    onMouseLeave={this.handleMouseLeave.bind(this)}
+                    width="219" 
+                    height="123">
+
+                        <source src={`${this.props.video.videoUrl}#t=3`} type="video/mp4" ></source>
+                    </video>
+                    <div className="video-control-bar">
+                        <button className="video-like-button"><i class="fas fa-plus-circle-light"></i></button>
+                    </div>
+                </div>
             </div>
            
 //  const vid = document.getElementById("video_item");
