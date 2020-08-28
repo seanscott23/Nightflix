d9942670eee11e406c4c3d95a0ef496742e1a611
thewizard23 <seanpscott6@gmail.com>
2020-08-27 19:59:53 -0700

    show page

--- frontend/components/videos/video_show.jsx
+++ frontend/components/videos/video_show.jsx
@@ -15,7 +15,17 @@ class VideoShow extends React.Component {
         }else{
             e.currentTarget.pause()
         }
-       
+    }
+
+    toggleFullscreen() {
+    let elem = document.querySelector("video");
+        elem.requestFullscreen();
+        if (document.fullscreenElement){
+            document.exitFullscreen();
+        }
+}
+    showTitle(e){
+        e.currentTarget.classList.toggle('active');
     }
 
 
@@ -28,14 +38,15 @@ class VideoShow extends React.Component {
         return (
            <div className="main-video-container">
                <div className="video-solo">
-                    <h1 className="main-title">{this.props.video.title}</h1>
-                    <div className="video-main">
+                    <div className="video-main" onClick={this.showTitle.bind(this)}>
                             <video
                             onClick={this.playVideo.bind(this)}
+                            onDoubleClick={this.toggleFullscreen.bind(this)}
                                 width="600"
                                 height="400">
                                 <source src={`${this.props.video.videoUrl}#t=3`} type="video/mp4" ></source>
                             </video>
+                        <h1 className="main-title active" >{this.props.video.title}</h1>
                     </div>
                     <div className="main-video-control-bar">
                         <button className="main-video-like-button"><i class="fas fa-plus-circle"></i></button>
