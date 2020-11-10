46595c696156238223b41bb89a9d6daa8b12578d
thewizard23 <seanpscott6@gmail.com>
2020-11-09 18:38:15 -0800

    Updated home page

--- frontend/components/videos/video_index.jsx
+++ frontend/components/videos/video_index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import VideosItem from './videos_item';
-
+import VideoPreview from './video_preview';
 
 class VideosIndex extends React.Component {
     constructor(props) {
@@ -26,7 +26,6 @@ class VideosIndex extends React.Component {
 
     changeCarouselIndex(category, number){
       const tempState = {}
-      
       if (number === 1){
         if(this.state[category] === 0){
           tempState[category] = (this.state[category] + 5);
@@ -34,37 +33,51 @@ class VideosIndex extends React.Component {
           tempState[category] =
             (this.state[category] + 5) % this.state[category];
         }
-        
-       debugger
         this.setState(tempState);
       }else {
         if(this.state[category] === 0){
             tempState[category] = (this.state[category] + 5) ;
-        }else{
+        } else{
        tempState[category] = (this.state[category] - 5) % this.state[category] ;
         }
-     debugger
           this.setState(tempState);
       }
-    
-     
     }
+ 
 
     render() {
-      
+     
         return (
           <div className="video-container">
-            <div className="genre-section">
-              <h1 className="genre-title">Action</h1>
-              <section className="carouselRow">
-                <div
-                  className="arrowButton"
-                  onClick={() => this.changeCarouselIndex("action", -1)}
-                >
-                  {" "}
-                  <i class="fas fa-chevron-left"></i>
+            <div>
+              {this.props.Drama.slice(
+                this.state.drama + 2,
+                this.state.drama + 3
+              ).map((video) => (
+                <div className="homeVideoPreview">
+                  <VideoPreview
+                    key={"action" + parseInt(video.id)}
+                    video={video}
+                    history={this.props.history}
+                    currentUser={this.props.currentUser}
+                  />
                 </div>
-                {this.props.Action.slice(this.state.action, this.state.action + 5).map((video) => (
+              ))}
+            </div>
+            <div className="genre-section-action">
+              <h1 className="genre-title">Action</h1>
+              <div
+                className="arrowButton"
+                onClick={() => this.changeCarouselIndex("action", -1)}
+              >
+                {" "}
+                <i class="fas fa-chevron-left"></i>
+              </div>
+              <section className="carouselRow">
+                {this.props.Action.slice(
+                  this.state.action,
+                  this.state.action + 6
+                ).map((video) => (
                   <div className="video-item">
                     <VideosItem
                       key={"action" + parseInt(video.id)}
@@ -77,9 +90,45 @@ class VideosIndex extends React.Component {
                     />
                   </div>
                 ))}
-                <div
+             
+              </section>
+                 <div
                   className="arrowButton"
                   onClick={() => this.changeCarouselIndex("action", 1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-right"></i>
+                </div>
+            </div>
+            <div className="genre-section">
+              <h1 className="genre-title">Comedies</h1>
+              <section className="carouselRow">
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("comedy", -1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-left"></i>
+                </div>
+                {this.props.Comedy.slice(
+                  this.state.comedy,
+                  this.state.comedy + 6
+                ).map((video) => (
+                  <div className="video-item">
+                    <VideosItem
+                      key={"comedy" + parseInt(video.id)}
+                      video={video}
+                      history={this.props.history}
+                      addToMyList={this.props.addToMyList}
+                      currentUser={this.props.currentUser}
+                      removeFromMyList={this.props.removeFromMyList}
+                      requestUserList={this.props.requestUserList}
+                    />
+                  </div>
+                ))}
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("comedy", 1)}
                 >
                   {" "}
                   <i class="fas fa-chevron-right"></i>
@@ -87,140 +136,249 @@ class VideosIndex extends React.Component {
               </section>
             </div>
             <div className="genre-section">
-              <h1 className="genre-title">Comedies</h1>
-              <div className="carouselRow">
-                {this.props.Comedy.map((video) => (
-                  <div className="video-item">
-                    <VideosItem
-                      key={"comedy" + parseInt(video.id)}
-                      video={video}
-                      addToMyList={this.props.addToMyList}
-                      currentUser={this.props.currentUser}
-                      removeFromMyList={this.props.removeFromMyList}
-                      requestUserList={this.props.requestUserList}
-                    />
-                  </div>
-                ))}
-              </div>
-              <div className="carouselRow">
-                {this.props.Comedy.map((video) => (
-                  <div className="video-item">
-                    <VideosItem
-                      key={"comedy" + parseInt(video.id)}
-                      video={video}
-                      addToMyList={this.props.addToMyList}
-                      currentUser={this.props.currentUser}
-                      removeFromMyList={this.props.removeFromMyList}
-                      requestUserList={this.props.requestUserList}
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-            <div className="genre-section">
               <h1 className="genre-title">Fantasy</h1>
-              {this.props.Fantasy.map((video) => (
-                <div className="video-item">
-                  <VideosItem
-                    key={"fantasy" + parseInt(video.id)}
-                    video={video}
-                    addToMyList={this.props.addToMyList}
-                    currentUser={this.props.currentUser}
-                    removeFromMyList={this.props.removeFromMyList}
-                    requestUserList={this.props.requestUserList}
-                  />
+              <section className="carouselRow">
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("fantasy", -1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-left"></i>
                 </div>
-              ))}
+                {this.props.Fantasy.slice(
+                  this.state.fantasy,
+                  this.state.fantasy + 6
+                ).map((video) => (
+                  <div className="video-item">
+                    <VideosItem
+                      key={"fantasy" + parseInt(video.id)}
+                      video={video}
+                      history={this.props.history}
+                      addToMyList={this.props.addToMyList}
+                      currentUser={this.props.currentUser}
+                      removeFromMyList={this.props.removeFromMyList}
+                      requestUserList={this.props.requestUserList}
+                    />
+                  </div>
+                ))}
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("fantasy", 1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-right"></i>
+                </div>
+              </section>
             </div>
             <div className="genre-section">
               <h1 className="genre-title">Drama</h1>
-              {this.props.Drama.map((video) => (
-                <div className="video-item">
-                  <VideosItem
-                    key={"drama" + parseInt(video.id)}
-                    video={video}
-                    addToMyList={this.props.addToMyList}
-                    currentUser={this.props.currentUser}
-                    removeFromMyList={this.props.removeFromMyList}
-                    requestUserList={this.props.requestUserList}
-                  />
+              <section className="carouselRow">
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("drama", -1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-left"></i>
                 </div>
-              ))}
+                {this.props.Drama.slice(
+                  this.state.drama,
+                  this.state.drama + 6
+                ).map((video) => (
+                  <div className="video-item">
+                    <VideosItem
+                      key={"drama" + parseInt(video.id)}
+                      video={video}
+                      history={this.props.history}
+                      addToMyList={this.props.addToMyList}
+                      currentUser={this.props.currentUser}
+                      removeFromMyList={this.props.removeFromMyList}
+                      requestUserList={this.props.requestUserList}
+                    />
+                  </div>
+                ))}
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("drama", 1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-right"></i>
+                </div>
+              </section>
             </div>
             <div className="genre-section">
               <h1 className="genre-title">Crime</h1>
-              {this.props.Crime.map((video) => (
-                <div className="video-item">
-                  <VideosItem
-                    key={"crime" + parseInt(video.id)}
-                    video={video}
-                    addToMyList={this.props.addToMyList}
-                    currentUser={this.props.currentUser}
-                    removeFromMyList={this.props.removeFromMyList}
-                    requestUserList={this.props.requestUserList}
-                  />
+              <section className="carouselRow">
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("crime", -1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-left"></i>
                 </div>
-              ))}
+                {this.props.Crime.slice(
+                  this.state.crime,
+                  this.state.crime + 6
+                ).map((video) => (
+                  <div className="video-item">
+                    <VideosItem
+                      key={"crime" + parseInt(video.id)}
+                      video={video}
+                      history={this.props.history}
+                      addToMyList={this.props.addToMyList}
+                      currentUser={this.props.currentUser}
+                      removeFromMyList={this.props.removeFromMyList}
+                      requestUserList={this.props.requestUserList}
+                    />
+                  </div>
+                ))}
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("crime", 1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-right"></i>
+                </div>
+              </section>
             </div>
             <div className="genre-section">
               <h1 className="genre-title">Science Fiction</h1>
-              {this.props.Science.map((video) => (
-                <div className="video-item">
-                  <VideosItem
-                    key={"science-fiction" + parseInt(video.id)}
-                    video={video}
-                    addToMyList={this.props.addToMyList}
-                    currentUser={this.props.currentUser}
-                    removeFromMyList={this.props.removeFromMyList}
-                    requestUserList={this.props.requestUserList}
-                  />
+              <section className="carouselRow">
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("science", -1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-left"></i>
                 </div>
-              ))}
+                {this.props.Science.slice(
+                  this.state.science,
+                  this.state.science + 6
+                ).map((video) => (
+                  <div className="video-item">
+                    <VideosItem
+                      key={"science-fiction" + parseInt(video.id)}
+                      video={video}
+                      history={this.props.history}
+                      addToMyList={this.props.addToMyList}
+                      currentUser={this.props.currentUser}
+                      removeFromMyList={this.props.removeFromMyList}
+                      requestUserList={this.props.requestUserList}
+                    />
+                  </div>
+                ))}
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("science", 1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-right"></i>
+                </div>
+              </section>
             </div>
             <div className="genre-section">
               <h1 className="genre-title">Thriller</h1>
-              {this.props.Thriller.map((video) => (
-                <div className="video-item">
-                  <VideosItem
-                    key={"thriller" + parseInt(video.id)}
-                    video={video}
-                    addToMyList={this.props.addToMyList}
-                    currentUser={this.props.currentUser}
-                    removeFromMyList={this.props.removeFromMyList}
-                    requestUserList={this.props.requestUserList}
-                  />
+              <section className="carouselRow">
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("thriller", -1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-left"></i>
                 </div>
-              ))}
+                {this.props.Thriller.slice(
+                  this.state.thriller,
+                  this.state.thriller + 6
+                ).map((video) => (
+                  <div className="video-item">
+                    <VideosItem
+                      key={"thriller" + parseInt(video.id)}
+                      video={video}
+                      history={this.props.history}
+                      addToMyList={this.props.addToMyList}
+                      currentUser={this.props.currentUser}
+                      removeFromMyList={this.props.removeFromMyList}
+                      requestUserList={this.props.requestUserList}
+                    />
+                  </div>
+                ))}
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("thriller", 1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-right"></i>
+                </div>
+              </section>
             </div>
             <div className="genre-section">
               <h1 className="genre-title">Family</h1>
-              {this.props.Family.map((video) => (
-                <div className="video-item">
-                  <VideosItem
-                    key={"family" + parseInt(video.id)}
-                    video={video}
-                    addToMyList={this.props.addToMyList}
-                    currentUser={this.props.currentUser}
-                    removeFromMyList={this.props.removeFromMyList}
-                    requestUserList={this.props.requestUserList}
-                  />
+              <section className="carouselRow">
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("family", -1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-left"></i>
                 </div>
-              ))}
+                {this.props.Family.slice(
+                  this.state.family,
+                  this.state.family + 6
+                ).map((video) => (
+                  <div className="video-item">
+                    <VideosItem
+                      key={"family" + parseInt(video.id)}
+                      video={video}
+                      history={this.props.history}
+                      addToMyList={this.props.addToMyList}
+                      currentUser={this.props.currentUser}
+                      removeFromMyList={this.props.removeFromMyList}
+                      requestUserList={this.props.requestUserList}
+                    />
+                  </div>
+                ))}
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("family", 1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-right"></i>
+                </div>
+              </section>
             </div>
             <div className="genre-section">
               <h1 className="genre-title">Romance</h1>
-              {this.props.Romance.map((video) => (
-                <div className="video-item">
-                  <VideosItem
-                    key={"romance" + parseInt(video.id)}
-                    video={video}
-                    addToMyList={this.props.addToMyList}
-                    currentUser={this.props.currentUser}
-                    removeFromMyList={this.props.removeFromMyList}
-                    requestUserList={this.props.requestUserList}
-                  />
+              <section className="carouselRow">
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("romance", -1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-left"></i>
                 </div>
-              ))}
+                {this.props.Romance.slice(
+                  this.state.romance,
+                  this.state.romance + 6
+                ).map((video) => (
+                  <div className="video-item">
+                    <VideosItem
+                      key={"romance" + parseInt(video.id)}
+                      video={video}
+                      history={this.props.history}
+                      addToMyList={this.props.addToMyList}
+                      currentUser={this.props.currentUser}
+                      removeFromMyList={this.props.removeFromMyList}
+                      requestUserList={this.props.requestUserList}
+                    />
+                  </div>
+                ))}
+                <div
+                  className="arrowButton"
+                  onClick={() => this.changeCarouselIndex("romance", 1)}
+                >
+                  {" "}
+                  <i class="fas fa-chevron-right"></i>
+                </div>
+              </section>
             </div>
           </div>
         );
---
+++ frontend/components/videos/video_preview.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+class VideoPreview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+    this.state = {
+      checked: false,
+    };
+  }
+
+  handleMouseEnter(e) {
+    e.currentTarget.play();
+  }
+
+  handleMouseLeave(e) {
+    e.currentTarget.pause();
+  }
+
+ 
+
+  render() {
+
+    return (
+      <div className="VideoPreview">
+        <h1 className="PreviewTitle">{this.props.video.title}</h1>
+        <span className="videoDescription">
+          Will Hunting, a janitor at M.I.T., has a gift for mathematics, but
+          needs help from a psychologist to find direction in his life.
+        </span>
+        <div
+          onClick={() =>
+            this.props.history.push(`/videos/${this.props.video.id}`)
+          }
+        >
+          <video
+            className="videoPreview"
+            onMouseEnter={(e) => this.handleMouseEnter(e)}
+            onMouseLeave={(e) => this.handleMouseLeave(e)}
+            width="1434"
+            height="600"
+          >
+            <source
+              src={`${this.props.video.videoUrl}#t=3`}
+              type="video/mp4"
+            ></source>
+          </video>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default VideoPreview;
